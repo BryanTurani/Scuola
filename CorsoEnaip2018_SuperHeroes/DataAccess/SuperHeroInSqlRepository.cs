cbe12327731cf2469697c87473fced735d2a5840
bryan.turani@hotmail.com <bryan.turani@hotmail.com>
2018-03-13 11:31:35 +0100

    SuperHeroes - SQL

---
+++ CorsoEnaip2018_SuperHeroes/DataAccess/SuperHeroInSqlRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorsoEnaip2018_SuperHeroes.DataAccess
+{
+    public class SuperHeroInSqlRepository
+    {
+        sqlconnection connectionString = new sqlconnection("");
+    }
+}
