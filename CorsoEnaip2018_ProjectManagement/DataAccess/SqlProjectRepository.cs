ffd6180d24003e7ed13a98b6ebcd353c9197e39b
Bryan Turani <bryan.turani@hotmail.com>
2018-03-23 12:56:47 +0100

    Commit stra figo

--- CorsoEnaip2018_ProjectManagement/DataAccess/SqlProjectRepository.cs
+++ CorsoEnaip2018_ProjectManagement/DataAccess/SqlProjectRepository.cs
@@ -10,53 +10,145 @@ namespace CorsoEnaip2018_ProjectManagement.DataAccess
 {
     public class SqlProjectRepository : IRepository<Project>
     {
-        // AL posto di IRepository<Villain> usare IRepository<Project>
-        // Villain => Project
-
-        private AppDbContext _context;
-
-        public SqlProjectRepository(AppDbContext context)
-        {
-            _context = context;
-        }
+        public const string CONNECTION_STRING = @"Data Source=TRISRV10\SQLEXPRESS;Database=CorsoEuris_Kraus;Integrated Security=True";
 
         public bool Delete(Project model)
         {
-            _context.Customers.Remove(model);
+            using (var conn = new SqlConnection(CONNECTION_STRING))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
 
-            var result = _context.SaveChanges();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "project_delete";
+                cmd.Parameters.AddWithValue("@id", model.Id);
 
-            return result == 1;
+                var result = cmd.ExecuteNonQuery();
+
+                return result == 1;
+            }
         }
 
         public Project Find(int id)
         {
-            //return _context..FirstOrDefault(x => x.Id == id);
+            using (var conn = new SqlConnection(CONNECTION_STRING))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "project_select";
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (var r = cmd.ExecuteReader())
+                {
+                    if (r.Read())
+                    {
+                        var p = new Project
+                        {
+                            Id = (int)r["Id"],
+                            Name = (string)r["Name"],
+                            //Customer = (string)r["Customer"],
+                            Manager = (string)r["Manager"],
+                            StartDate = (DateTime)r["StartDate"],
+                            EndDate = (DateTime)r["EndDate"],
+                            DeliveryDate = (DateTime)r["DeliveryDate"],
+                            Price = (decimal)r["Price"],
+                            Cost = (decimal)r["Cost"],
+                        };
+
+                        return p;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
         }
 
         public List<Project> FindAll()
         {
-            var models = _context.Customers
-                .Include(x => x.Nemesis)
-                .ToList();
+            using (var conn = new SqlConnection(CONNECTION_STRING))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
 
-            return models;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "project_select_all";
+
+                using (var r = cmd.ExecuteReader())
+                {
+                    var list = new List<Project>();
+
+                    while (r.Read())
+                    {
+                        var p = new Project
+                        {
+                            Id = (int)r["Id"],
+                            Name = (string)r["Name"],
+                            //Customer = (string)r["Customer"],
+                            Manager = (string)r["Manager"],
+                            StartDate = (DateTime)r["StartDate"],
+                            EndDate = (DateTime)r["EndDate"],
+                            DeliveryDate = (DateTime)r["DeliveryDate"],
+                            Price = (decimal)r["Price"],
+                            Cost = (decimal)r["Cost"],
+                        };
+
+                        list.Add(p);
+                    }
+
+                    return list;
+                }
+            }
         }
 
         public void Insert(Project model)
         {
-            _context.Customers.Add(model);
+            using (var conn = new SqlConnection(CONNECTION_STRING))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
 
-            _context.SaveChanges();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "project_insert";
+                cmd.Parameters.AddWithValue("@name", model.Name);
+                cmd.Parameters.AddWithValue("@customer", model.Customer);
+                cmd.Parameters.AddWithValue("@manager", model.Manager);
+                cmd.Parameters.AddWithValue("@start", model.StartDate);
+                cmd.Parameters.AddWithValue("@end", model.EndDate);
+                cmd.Parameters.AddWithValue("@delivery", model.DeliveryDate);
+                cmd.Parameters.AddWithValue("@price", model.Price);
+                cmd.Parameters.AddWithValue("@cost", model.Cost);
+
+                cmd.ExecuteNonQuery();
+            }
         }
 
         public bool Update(Project model)
         {
-            _context.Customers.Update(model);
+            using (var conn = new SqlConnection(CONNECTION_STRING))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
 
-            var result = _context.SaveChanges();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "project_update";
+                cmd.Parameters.AddWithValue("@id", model.Id);
+                cmd.Parameters.AddWithValue("@name", model.Name);
+                cmd.Parameters.AddWithValue("@customer", model.Customer);
+                cmd.Parameters.AddWithValue("@manager", model.Manager);
+                cmd.Parameters.AddWithValue("@start", model.StartDate);
+                cmd.Parameters.AddWithValue("@end", model.EndDate);
+                cmd.Parameters.AddWithValue("@delivery", model.DeliveryDate);
+                cmd.Parameters.AddWithValue("@price", model.Price);
+                cmd.Parameters.AddWithValue("@cost", model.Cost);
 
-            return result == 1;
+                var result = cmd.ExecuteNonQuery();
+
+                return result == 1;
+            }
         }
     }
 }
--- CorsoEnaip2018_ProjectManagement/Models/Project.cs
+++ CorsoEnaip2018_ProjectManagement/Models/Project.cs
@@ -2,46 +2,25 @@
 
 namespace CorsoEnaip2018_ProjectManagement.Models
 {
-    // Invece di SqlReader e SqlCommand, usare EntityFramework
-    // Per l'Index di Project, creare un ViewModel solo con i dati necessari
-    // A scelta, Customer o Manager devono essere entità esterne
-    // Collegate da una relazione uno-a-molti (foreign-key) con la classe Project.
-    // Sulla View di Edit, usare un 'select' per selezionare Customer/Manager.
-
+// Invece di SqlReader e SqlCommand, usare EntityFramework
+// Per l'Index di Project, creare un ViewModel solo coi dati necessari
+// A scelta, Customer o Manager devono essere entità esterne,
+// collegate da una relazione uno-a-molti con la classe Project.
+// Sulla View di Edit, usare un 'select' per selezionare Customer/Manager
     public class Project
     {
         public Project() { }
 
-        public Project(
-            int id,
-            string name,
-            string customer,
-            string manager,
-            DateTime startDate,
-            DateTime endDate,
-            DateTime deliveryDate,
-            decimal price,
-            decimal cost)
-        {
-            Id = id;
-            Name = name;
-            Customer = customer;
-            Manager = manager;
-            StartDate = startDate;
-            EndDate = endDate;
-            DeliveryDate = deliveryDate;
-            Price = price;
-            Cost = cost;
-        }
-
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Customer { get; set; }
         public string Manager { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public DateTime DeliveryDate { get; set; }
         public decimal Price { get; set; }
         public decimal Cost { get; set; }
+
+        public int CustomerId { get; set; }
+        public Company Customer { get; set; }
     }
 }
