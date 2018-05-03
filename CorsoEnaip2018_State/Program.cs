376c49035d1e30e09dc9029b122e3ce456b5f408
Bryan Turani <bryan.turani@hotmail.com>
2018-05-03 12:16:20 +0200

    Modifiche

---
+++ CorsoEnaip2018_State/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CorsoEnaip2018_State
+{
+    /* ParkChecker
+     * - All'inizio è chiuso
+     * - Se si inserisce una moneta si apre
+     * - Se passa un'auto si chiude
+     * - Se si inseriscono altre monete vengono restituite
+     * - Se si prova a chiuderlo 2 volte non succede niente
+     * - Se prova a passare una seconda auto, non può
+     * 
+     * InsertCoin()
+     * CarTriesToPass()
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
